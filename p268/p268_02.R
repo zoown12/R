a4e604d61205e8e24548e752ae318fdc081c32b4
zoown12 <96428476+zoown12@users.noreply.github.com>
2022-10-12 11:30:11 +0900

    Add files via upload

--- p268/p268_02.R
+++ p268/p268_02.R
@@ -1,9 +1,8 @@
-encoding{UTF8}
 useNIADic() 
 library(KoNLP)
 extractNoun("나는 지금 R을 공부하고 있다.")
 
-txt=readLines("hiphop.txt")
+txt=readLines("hiphop.txt",encoding="UTF-8")
 head(txt,30)
 
 library(stringr)
@@ -20,3 +19,12 @@ noun=extractNoun(t1)
 word_cnt=table(unlist(noun))
 
 df_word=as.data.frame(word_cnt,stringsAsFactors = F)
+
+#=====p269===
+library(KoNLP)
+useNIADic()
+ex1=extractNoun("우리는 지금 10장 텍스트마이닝을 공부하고 있다. 10장은 마이닝 공부입니다.")
+head(ex1)
+
+word_ex1=table(unlist(ex1))
+df_word_ex1=as.data.frame(word_ex1,stringsAsFactors = F)
