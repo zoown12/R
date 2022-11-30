a85dd42d9a21d0a72253038e4b2861c2bddb87cc
zoown12 <96428476+zoown12@users.noreply.github.com>
2022-11-30 13:35:50 +0900

    Add files via upload

---
+++ p280/284_1.R
@@ -0,0 +1,48 @@
+install.packages("DT")
+library(DT)
+library(kormaps2014)
+library(stringi)
+library(dplyr)
+library(ggiraphExtra)
+library(ggplot2)
+
+korpop1 = kormaps2014::korpop1
+korpop2 = kormaps2014::korpop2
+korpop3 = kormaps2014::korpop3
+str(changeCode(korpop1))
+
+korpop1 <-rename(korpop1,pop=총인구_명 , name = 행정구역별_읍면동)
+korpop3 <-rename(korpop3,pop=총인구_명 , name = 행정구역별_읍면동)
+
+korpop1$name<-iconv(korpop1$name,"UTF-8","CP949")
+
+korpop1_incheon <-korpop1$name
+
+str(changeCode(kormap1))
+
+areacode
+
+
+#인천광역시
+
+areacode
+korpop2 <-rename(korpop2,pop=총인구_명 , name = 행정구역별_읍면동)
+
+korpop2_incheon <-korpop2 %>% filter(code<24000 & code>23000) 
+
+ggChoropleth(data = korpop2_incheon,
+             aes(fill=pop,
+                 map_id=code,
+                 tooltip=name),
+             map=kormap2,
+             interactive = T)
+
+a1<-ggplot(korpop2_incheon, 
+       aes(map_id=code, fill=pop))+
+  geom_map(map=kormap2,colour="black",size=0.1)+
+  expand_limits(x=kormap2$long, y=kormap2$lat)+
+  scale_fill_gradientn(colours = c("white","orange","red"))+
+  ggtitle("인천광역시 ggplot")+
+  coord_map()
+
+a1
--- p280/p282_1.R
+++ p280/p282_1.R
@@ -37,6 +37,9 @@ install.packages("stringi")
 install.packages("devtools")
 devtools::install_github("cardiomoon/kormaps2014")
 library(kormaps2014)
+
+
+
 str(korpop1)
 str(changeCode(korpop1))
 
@@ -45,3 +48,20 @@ korpop1=rename(korpop1, pop=총인구_명,name=행정구역별_읍면동)
 korpop1$name = iconv(korpop1$name,"UTF-8","cp949")
 
 str(changeCode(kormap1))
+
+# korpop1 , korpop2 , korpop3 , kormap1, kormap2, kormap3
+
+#p287 단계구분도
+
+library(ggiraphExtra)
+
+library(ggplot2)
+
+ggChoropleth(data = korpop1,
+             aes(fill=pop,
+                 map_id=code,
+                 tooltip=name),
+             map=kormap1,
+             interactive = T)
+
+#인천광역시 
