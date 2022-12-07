d6918c9b48a96ec45f41d8cc04cb558bbdda83f4
zoown12 <96428476+zoown12@users.noreply.github.com>
2022-12-07 12:09:28 +0900

    Add files via upload

--- p280/284_1.R
+++ p280/284_1.R
@@ -10,6 +10,8 @@ korpop1 = kormaps2014::korpop1
 korpop2 = kormaps2014::korpop2
 korpop3 = kormaps2014::korpop3
 str(changeCode(korpop1))
+kormap2 = kormaps2014::kormap2
+kormap3 = kormaps2014::kormap3
 
 korpop1 <-rename(korpop1,pop=총인구_명 , name = 행정구역별_읍면동)
 korpop3 <-rename(korpop3,pop=총인구_명 , name = 행정구역별_읍면동)
@@ -23,7 +25,7 @@ str(changeCode(kormap1))
 areacode
 
 
-#인천광역시
+#인천광역시 #11장,6장
 
 areacode
 korpop2 <-rename(korpop2,pop=총인구_명 , name = 행정구역별_읍면동)
@@ -46,3 +48,22 @@ a1<-ggplot(korpop2_incheon,
   coord_map()
 
 a1
+
+#읍,면,동
+korpop3_incheon <- korpop3 %>% filter(code<2400000 & code>2300000)
+ggChoropleth(data = korpop3_incheon,
+             aes(fill=pop,
+                 map_id=code,
+                 tooltip=name),
+             map=kormap3,
+             interactive = T)
+
+areacode
+a2<-ggplot(korpop3_incheon, 
+           aes(map_id=code, fill=pop))+
+  geom_map(map=kormap3,colour="black",size=0.1)+
+  expand_limits(x=kormap3$long, y=kormap3$lat)+
+  scale_fill_gradientn(colours = c("white","orange","red"))+
+  ggtitle("인천광역시(읍/면/동) ggplot")+
+  coord_map()
+a2
